260b382248687fc4a768c39b742e48cdfa6ef324
usama <sohiusam1@gmail.com>
2024-04-03 10:40:46 +0500

    usama

--- lib/screens/auth/sign_up.dart
+++ lib/screens/auth/sign_up.dart
@@ -19,9 +19,9 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-    bool _obscureText = true;
+  bool _obscureText = true;
   bool _obscureText1 = true;
-    void _toggle() {
+  void _toggle() {
     setState(() {
       _obscureText = !_obscureText;
     });
@@ -32,160 +32,171 @@ class _RegisterScreenState extends State<RegisterScreen> {
       _obscureText1 = !_obscureText1;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: GetBuilder<AuthController>(
-            builder: (controller) => Stack(
-                  children: [
-                    Image.asset(
-                      "assets/images/loginback.jpg",
-                      height: MediaQuery.of(context).size.height * 0.25,
-                      width: double.infinity,
-                    ),
-                    Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
-                          child: Center(
-                            child: Image(
-                              image: AssetImage("assets/images/splashLogo.png"),
-                              height: 100,
-                              color: white,
-                            ),
-                          ),
-                        ),
-                        Container(
-                            padding: EdgeInsets.only(left: 20, right: 20),
-                            height: MediaQuery.of(context).size.height * 0.72,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                                color: Color.fromARGB(255, 250, 250, 250),
-                                borderRadius: BorderRadius.only(
-                                    topRight: Radius.circular(30),
-                                    topLeft: Radius.circular(30))),
-                            child: SingleChildScrollView(
-                              child: Padding(
-                                padding: MediaQuery.of(context).viewInsets,
-                                child: Column(children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 30, bottom: 20),
-                                    child: Text(
-                                      'Register Account',
-                                      style: TextStyle(
-                                          fontSize: 24,
-                                          fontWeight: FontWeight.w600,
-                                          color: greenish),
-                                    ),
-                                  ),
-                                  InputField1(
-                                    hint: 'Username',
-                                    icon: 'assets/images/user.svg',
-                                    controller: authController.userName,
-                                    validator: (user) =>
-                                        Validators.emptyStringValidator(
-                                            user, 'UserName'),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        bottom: 4, top: 4),
-                                    child: InputField1(
-                                      icon: 'assets/images/email.svg',
-                                      hint: 'Email Address',
-                                      controller: authController.email,
-                                      validator: (user) =>
-                                          Validators.emptyStringValidator(
-                                              user, 'email'),
-                                      type: TextInputType.emailAddress,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 4),
-                                    child: InputField1(
-                                      hint: 'Phone Number',
-                                      icon: 'assets/images/phone.svg',
-                                      controller: authController.phone,
-                                      validator: (user) =>
-                                          Validators.emptyStringValidator(
-                                              user, 'Number'),
-                                      type: TextInputType.number,
-                                    ),
-                                  ),
-                                  // InputField1(
-                                  //   hint: 'Password',
-                                  //   icon: 'assets/images/lock.svg',
-                                  //   obscure: true,
-                                  //   controller: authController.password,
-                                  //   validator: (password) =>
-                                  //       Validators.passwordValidator(password),
-                                  // ),
-                                  // Padding(
-                                  //   padding: const EdgeInsets.only(
-                                  //       top: 4, bottom: 20),
-                                  //   child: InputField1(
-                                  //     hint: 'Confirm Password',
-                                  //     icon: 'assets/images/lock.svg',
-                                  //     obscure: true,
-                                  //     controller:
-                                  //         authController.confirmPassword,
-                                  //     validator: (password) =>
-                                  //         Validators.passwordValidator(
-                                  //             password),
-                                  //   ),
-                                  // ),
-                                  InputFieldPassword1(
-                                    controller: authController.password,
-                                    imageIcon: 'assets/images/lock.svg',
-                                    hint: 'Password',
-                                    borderColor: Colors.black,
-                                    imageColor: Colors.black,
-                                    toggle: _toggle,
-                                    obscure: _obscureText,
-                                    validate: authController.validateSignUpForm,
-                                    validator: (password) =>
-                                        Validators.passwordValidator(password),
-                                  ),
-                          Padding(
-                            padding: const EdgeInsets.only(top:4,bottom: 20),
-                            child: InputFieldPassword1(
-                              controller: authController.confirmPassword,
-                              imageIcon: 'assets/images/lock.svg',
-                              hint: 'Confirm Password',
-                              borderColor: Colors.black,
-                              imageColor: Colors.black,
-                              toggle: _toggle1,
-                              obscure: _obscureText1,
-                              validate: authController.validateSignUpForm,
-                              validator: (password) =>
-                                  Validators.passwordValidator(password),
-                            ),
-                          ),
-                                  LargeButton(
-                                    title: 'Sign Up',
-                                    sreenRatio: 0.9,
-                                    onPressed: () {
-                                      authController.SignUp((success) {
-                                        if (success) {
-                                          authController.ClearSignupVariables();
-                                          authController.validateSignUpForm =
-                                              false.obs;
-                                          Get.offAll(() => Home_screen());
-                                        }
-                                      });
-                                      setState(() {});
-                                    },
-                                    color: greenish,
-                                    textcolor: Colors.white,
-                                    buttonWidth: 0.95,
-                                  ),
-                                ]),
+            builder: (controller) => SingleChildScrollView(
+                  child: Stack(
+                    children: [
+                      Image.asset(
+                        "assets/images/loginback.jpg",
+                        height: MediaQuery.of(context).size.height * 0.25,
+                        width: double.infinity,
+                      ),
+                      Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                            child: Center(
+                              child: Image(
+                                image:
+                                    AssetImage("assets/images/splashLogo.png"),
+                                height: 100,
+                                color: white,
                               ),
-                            ))
-                      ],
-                    ),
-                  ],
+                            ),
+                          ),
+                          Container(
+                              padding: EdgeInsets.only(left: 20, right: 20),
+                              height: MediaQuery.of(context).size.height * 0.72,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 250, 250, 250),
+                                  borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(30),
+                                      topLeft: Radius.circular(30))),
+                              child: SingleChildScrollView(
+                                child: Padding(
+                                  padding: MediaQuery.of(context).viewInsets,
+                                  child: Column(children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 30, bottom: 20),
+                                      child: Text(
+                                        'Register Account',
+                                        style: TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.w600,
+                                            color: greenish),
+                                      ),
+                                    ),
+                                    InputField1(
+                                      hint: 'Username',
+                                      icon: 'assets/images/user.svg',
+                                      controller: authController.userName,
+                                      validator: (user) =>
+                                          Validators.emptyStringValidator(
+                                              user, 'UserName'),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          bottom: 4, top: 4),
+                                      child: InputField1(
+                                        icon: 'assets/images/email.svg',
+                                        hint: 'Email Address',
+                                        controller: authController.email,
+                                        validator: (user) =>
+                                            Validators.emptyStringValidator(
+                                                user, 'email'),
+                                        type: TextInputType.emailAddress,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 4),
+                                      child: InputField1(
+                                        hint: 'Phone Number',
+                                        icon: 'assets/images/phone.svg',
+                                        controller: authController.phone,
+                                        validator: (user) =>
+                                            Validators.emptyStringValidator(
+                                                user, 'Number'),
+                                        type: TextInputType.number,
+                                      ),
+                                    ),
+                                    // InputField1(
+                                    //   hint: 'Password',
+                                    //   icon: 'assets/images/lock.svg',
+                                    //   obscure: true,
+                                    //   controller: authController.password,
+                                    //   validator: (password) =>
+                                    //       Validators.passwordValidator(password),
+                                    // ),
+                                    // Padding(
+                                    //   padding: const EdgeInsets.only(
+                                    //       top: 4, bottom: 20),
+                                    //   child: InputField1(
+                                    //     hint: 'Confirm Password',
+                                    //     icon: 'assets/images/lock.svg',
+                                    //     obscure: true,
+                                    //     controller:
+                                    //         authController.confirmPassword,
+                                    //     validator: (password) =>
+                                    //         Validators.passwordValidator(
+                                    //             password),
+                                    //   ),
+                                    // ),
+                                    InputFieldPassword1(
+                                      controller: authController.password,
+                                      imageIcon: 'assets/images/lock.svg',
+                                      hint: 'Password',
+                                      borderColor: Colors.black,
+                                      imageColor: Colors.black,
+                                      toggle: _toggle,
+                                      obscure: _obscureText,
+                                      validate:
+                                          authController.validateSignUpForm,
+                                      validator: (password) =>
+                                          Validators.passwordValidator(
+                                              password),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 4, bottom: 20),
+                                      child: InputFieldPassword1(
+                                        controller:
+                                            authController.confirmPassword,
+                                        imageIcon: 'assets/images/lock.svg',
+                                        hint: 'Confirm Password',
+                                        borderColor: Colors.black,
+                                        imageColor: Colors.black,
+                                        toggle: _toggle1,
+                                        obscure: _obscureText1,
+                                        validate:
+                                            authController.validateSignUpForm,
+                                        validator: (password) =>
+                                            Validators.passwordValidator(
+                                                password),
+                                      ),
+                                    ),
+                                    LargeButton(
+                                      title: 'Sign Up',
+                                      sreenRatio: 0.9,
+                                      onPressed: () {
+                                        authController.SignUp((success) {
+                                          if (success) {
+                                            authController
+                                                .ClearSignupVariables();
+                                            authController.validateSignUpForm =
+                                                false.obs;
+                                            Get.offAll(() => Home_screen());
+                                          }
+                                        });
+                                        setState(() {});
+                                      },
+                                      color: greenish,
+                                      textcolor: Colors.white,
+                                      buttonWidth: 0.95,
+                                    ),
+                                  ]),
+                                ),
+                              ))
+                        ],
+                      ),
+                    ],
+                  ),
                 )),
       ),
     );
