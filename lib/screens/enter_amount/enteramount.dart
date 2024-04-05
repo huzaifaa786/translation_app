aab1ac0f0ab15cb372ce6979a796c3c39b14c8ed
usama <sohiusam1@gmail.com>
2024-04-05 10:28:12 +0500

    usama

--- lib/screens/enter_amount/enteramount.dart
+++ lib/screens/enter_amount/enteramount.dart
@@ -8,7 +8,7 @@ import 'package:translation/static/input_field.dart';
 import 'package:translation/static/large_button.dart';
 import 'package:translation/static/titletopbar.dart';
 import 'package:translation/values/controllers.dart';
-
+import 'package:gap/gap.dart';
 import '../../../../values/colors.dart';
 
 class Topup_screen extends StatefulWidget {
@@ -33,166 +33,227 @@ class _Topup_screenState extends State<Topup_screen> {
     return Scaffold(
       body: SafeArea(
         child: GetBuilder<AmountController>(
-          builder: (controller) => SingleChildScrollView(
-            child: Column(
-              children: [
-                TitleTopbar(
-                  text: 'Enter amount'.tr,
-                  ontap: () {
-                    Get.back();
-                  },
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                InkWell(
-                  onTap: () {
-                    Enteramount();
-                  },
-                  child: Center(
-                      child: Text(
-                    amountController.Selectedvalue!,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 31),
-                  )),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Center(
-                    child: Text(
-                  "AED",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: greenish),
-                )),
-                SizedBox(
-                  height: 20,
-                ),
-                Center(
-                    child: Text(
-                  "or quick select amount".tr,
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-                )),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Box(
-                        title: '40',
-                        ontap: () {
-                          setState(() {
-                            amountController.Selectedvalue = '40';
-                          });
-                        },
-                        selected: amountController.Selectedvalue == '40'
-                            ? true
-                            : false,
-                      ),
-                      Box(
-                        title: '50',
-                        ontap: () {
-                          setState(() {
-                            amountController.Selectedvalue = '50';
-                          });
-                        },
-                        selected: amountController.Selectedvalue == '50'
-                            ? true
-                            : false,
-                      ),
-                      Box(
-                        title: '60',
-                        ontap: () {
-                          setState(() {
-                            amountController.Selectedvalue = '60';
-                          });
-                        },
-                        selected: amountController.Selectedvalue == '60'
-                            ? true
-                            : false,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30,right: 30),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Payment Method".tr,
-                        style: TextStyle(
-                            fontSize: 17, fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Directionality(
-                  textDirection: TextDirection.ltr,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 15, right: 15),
-                    child: Column(children: [
-                      // PPaymentMethod(
-                      //   title: 'Googlepay',
-                      //   image: "assets/icons/googlepay.png",
-                      //   groupvalue: _site,
-                      //   value: payMethod.googlePay,
-                      //   onchaged: () {
-                      //     toggleplan(payMethod.googlePay);
-                      //   },
-                      // ),
-                      // PPaymentMethod(
-                      //   title: 'Applepay',
-                      //   image: "assets/icons/applepay.png",
-                      //   groupvalue: _site,
-                      //   value: payMethod.applePay,
-                      //   onchaged: () {
-                      //     toggleplan(payMethod.applePay);
-                      //   },
-                      // ),
-                      // PPaymentMethod(
-                      //   title: 'Form Wallet',
-                      //   image: "assets/icons/walletpay.png",
-                      //   groupvalue: _site,
-                      //   value: payMethod.walletpay,
-                      //   onchaged: () {
-                      //     toggleplan(payMethod.walletpay);
-                      //   },
-                      // ),
-                      PPaymentMethod(
-                        title: 'Credit/visa card'.tr,
-                        image: "assets/icons/visapay.png",
-                        groupvalue: _site,
-                        value: payMethod.materCard,
-                        onchaged: () {
-                          toggleplan(payMethod.materCard);
-                        },
-                      ),
-                    ]),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Center(
-                      child: LargeButton(
-                    title: 'Top Up'.tr,
-                    sreenRatio: 0.9,
-                    onPressed: () {
-                      amountController.confirmPayment();
+          builder: (controller) => Stack(
+            children: [
+              Column(
+                children: [
+                  TitleTopbar(
+                    text: 'Top Up'.tr,
+                    ontap: () {
+                      Get.back();
                     },
-                    color: greenish,
-                    textcolor: Colors.white,
-                    buttonWidth: 0.95,
-                  )),
+                  ),
+                  Container(
+                    color: white,
+                    height: Get.height * 0.7,
+                    width: Get.width,
+                  )
+                ],
+              ),
+              Positioned(
+                top: Get.height * 0.08,
+                left: 12,
+                right: 12,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: white,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Enter Amount',
+                        style: TextStyle(
+                            color: greenish,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Gap(10),
+                      InkWell(
+                        onTap: () {
+                          Enteramount();
+                        },
+                        child: Center(
+                            child: Text(
+                          amountController.Selectedvalue!,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: greenish),
+                        )),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Center(
+                          child: Text(
+                        "AED",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15,
+                            color: Colors.black),
+                      )),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                          child: Text(
+                        "or quick select amount".tr,
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w400),
+                      )),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Box(
+                              title: '40',
+                              ontap: () {
+                                setState(() {
+                                  amountController.Selectedvalue = '40';
+                                });
+                              },
+                              selected: amountController.Selectedvalue == '40'
+                                  ? true
+                                  : false,
+                            ),
+                            Box(
+                              title: '50',
+                              ontap: () {
+                                setState(() {
+                                  amountController.Selectedvalue = '50';
+                                });
+                              },
+                              selected: amountController.Selectedvalue == '50'
+                                  ? true
+                                  : false,
+                            ),
+                            Box(
+                              title: '60',
+                              ontap: () {
+                                setState(() {
+                                  amountController.Selectedvalue = '60';
+                                });
+                              },
+                              selected: amountController.Selectedvalue == '60'
+                                  ? true
+                                  : false,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30, right: 30),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Payment Method".tr,
+                              style: TextStyle(
+                                  fontSize: 17, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Directionality(
+                        textDirection: TextDirection.ltr,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15, right: 15),
+                          child: Column(children: [
+                            // PPaymentMethod(
+
+                            //   title: 'Googlepay',
+
+                            //   image: "assets/icons/googlepay.png",
+
+                            //   groupvalue: _site,
+
+                            //   value: payMethod.googlePay,
+
+                            //   onchaged: () {
+
+                            //     toggleplan(payMethod.googlePay);
+
+                            //   },
+
+                            // ),
+
+                            // PPaymentMethod(
+
+                            //   title: 'Applepay',
+
+                            //   image: "assets/icons/applepay.png",
+
+                            //   groupvalue: _site,
+
+                            //   value: payMethod.applePay,
+
+                            //   onchaged: () {
+
+                            //     toggleplan(payMethod.applePay);
+
+                            //   },
+
+                            // ),
+
+                            // PPaymentMethod(
+
+                            //   title: 'Form Wallet',
+
+                            //   image: "assets/icons/walletpay.png",
+
+                            //   groupvalue: _site,
+
+                            //   value: payMethod.walletpay,
+
+                            //   onchaged: () {
+
+                            //     toggleplan(payMethod.walletpay);
+
+                            //   },
+
+                            // ),
+
+                            PPaymentMethod(
+                              title: 'Credit/visa card'.tr,
+                              image: "assets/icons/visapay.png",
+                              groupvalue: _site,
+                              value: payMethod.materCard,
+                              onchaged: () {
+                                toggleplan(payMethod.materCard);
+                              },
+                            ),
+                          ]),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Center(
+                            child: LargeButton(
+                          title: 'Top Up'.tr,
+                          sreenRatio: 0.9,
+                          onPressed: () {
+                            amountController.confirmPayment();
+                          },
+                          color: greenish,
+                          textcolor: Colors.white,
+                          buttonWidth: 0.95,
+                        )),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
