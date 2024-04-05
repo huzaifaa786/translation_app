6507dd934b7826948fa78f4594c209192bf59f2d
usama <sohiusam1@gmail.com>
2024-04-05 15:08:23 +0500

    usama

--- lib/static/chart.dart
+++ lib/static/chart.dart
@@ -22,77 +22,88 @@ class ChartCards extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
         onTap: onPressed,
-        child: Row(
-          children: <Widget>[
-            // ClipRRect(
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                // ClipRRect(
 
-            // borderRadius: BorderRadius.circular(40.0),
-            // child: Image.network('http://10.0.2.2:8000/api/image',
-            // height: 100,
-            //   width:100,
-            //    fit: BoxFit.cover,
-            //       )),
-            Container(
-              width: 70,
-              height: 70,
-              margin: EdgeInsets.only(left: 15,right: 15),
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                border: Border.all(width: 2, color: Color(0xFF34607B)),
-                borderRadius: BorderRadius.circular(70),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(22),
-                child: imgicon == ''
-                    ? Image(
-                        image: AssetImage('assets/images/5907.jpg'),
-                        height: 64,
-                        width: 64,
-                      )
-                    : CachedNetworkImage(
-                        imageUrl:
-                            'https://translation.klickwash.net/' + imgicon,
-                        height: 64,
-                        width: 64,
-                      ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // borderRadius: BorderRadius.circular(40.0),
+                // child: Image.network('http://10.0.2.2:8000/api/image',
+                // height: 100,
+                //   width:100,
+                //    fit: BoxFit.cover,
+                //       )),
+                Container(
+                  width: 70,
+                  height: 70,
+                  margin: EdgeInsets.only(left: 15, right: 15),
+                  // padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    // border: Border.all(width: 2, color: Color(0xFF34607B)),
+                    borderRadius: BorderRadius.circular(70),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(22),
+                    child: imgicon == ''
+                        ? Image(
+                            image: AssetImage('assets/images/5907.jpg'),
+                            height: 64,
+                            width: 64,
+                            fit: BoxFit.fill,
+                          )
+                        : CachedNetworkImage(
+                            imageUrl:
+                                'https://translation.klickwash.net/' + imgicon,
+                            height: 64,
+                            width: 64,
+                          ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(
-                          name,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600, fontSize: 14.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              name,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 14.0),
+                            ),
+                            // Text(
+                            //   duration,
+                            //   style: TextStyle(
+                            //     color: Colors.black45,
+                            //     fontSize: 12,
+                            //     fontWeight: FontWeight.w700,
+                            //   ),
+                            // ),
+                          ],
                         ),
-                        // Text(
-                        //   duration,
-                        //   style: TextStyle(
-                        //     color: Colors.black45,
-                        //     fontSize: 12,
-                        //     fontWeight: FontWeight.w700,
-                        //   ),
-                        // ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 9.0),
+                          child: Text(
+                            msg,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black45,
+                                fontSize: 14.0),
+                          ),
+                        )
                       ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 9.0),
-                      child: Text(
-                        msg,
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black45,
-                            fontSize: 14.0),
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Divider(
+                thickness: 1,
               ),
             )
           ],
--- lib/static/chattopbar.dart
+++ lib/static/chattopbar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:translation/screens/chat/chats.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:translation/values/colors.dart';
 
 class ChatTopBar extends StatelessWidget {
   const ChatTopBar({super.key, this.name, this.img});
@@ -11,100 +13,111 @@ class ChatTopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+        height: Get.height * 0.15,
+        decoration: BoxDecoration(
+          color: greenish,
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25)),
+        ),
         child: Padding(
-      padding: const EdgeInsets.only(left: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
+          padding: const EdgeInsets.only(left: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: SvgPicture.asset("assets/icons/back.svg")),
-              Container(
-                width: 55,
-                height: 55,
-                margin: EdgeInsets.only(left: 12),
-                padding: EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Color(0xFF34607B)),
-                  borderRadius: BorderRadius.circular(70),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(22),
-                  child: img == 'https://translation.ezmoveportal.com/'
-                      ? Image(
-                          image: AssetImage('assets/images/5907.jpg'),
-                          height: 64,
-                          width: 64,
-                        )
-                      : CachedNetworkImage(
-                          imageUrl: img,
-                          height: 64,
-                          width: 64,
-                        ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 12.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.35,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        name,
-                        style: TextStyle(
-                            fontSize: 19, fontWeight: FontWeight.w500),
-                      ),
-                      // Row(
-                      //   children: [
-                      //     Container(
-                      //       decoration: BoxDecoration(
-                      //         color: Color(0xFF34607B),
-                      //         borderRadius: BorderRadius.circular(999),
-                      //       ),
-                      //       height: 12,
-                      //       width: 12,
-                      //     ),
-                      //     Padding(
-                      //       padding: const EdgeInsets.only(left: 5.0),
-                      //       child: Text(
-                      //         'Online',
-                      //         style: TextStyle(
-                      //           fontFamily: 'Sk-Modernist',
-                      //           fontWeight: FontWeight.w400,
-                      //           fontSize: 12,
-                      //           height: 1.5,
-                      //           color: Color(0xFF34607B),
-                      //         ),
-                      //       ),
-                      //     ),
-                      //   ],
-                      // ),
-                    ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: SvgPicture.asset("assets/icons/back.svg")),
+                  Container(
+                    width: 70,
+                    height: 70,
+                    margin: EdgeInsets.only(left: 12),
+                    // padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: white),
+                      borderRadius: BorderRadius.circular(70),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(32),
+                      child: img == 'https://translation.ezmoveportal.com/'
+                          ? Image(
+                              image: AssetImage('assets/images/5907.jpg'),
+                              height: 64,
+                              width: 64,
+                            )
+                          : CachedNetworkImage(
+                              imageUrl: img,
+                              height: 64,
+                              width: 64,
+                            ),
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.35,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            name,
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: white),
+                          ),
+                          // Row(
+                          //   children: [
+                          //     Container(
+                          //       decoration: BoxDecoration(
+                          //         color: Color(0xFF34607B),
+                          //         borderRadius: BorderRadius.circular(999),
+                          //       ),
+                          //       height: 12,
+                          //       width: 12,
+                          //     ),
+                          //     Padding(
+                          //       padding: const EdgeInsets.only(left: 5.0),
+                          //       child: Text(
+                          //         'Online',
+                          //         style: TextStyle(
+                          //           fontFamily: 'Sk-Modernist',
+                          //           fontWeight: FontWeight.w400,
+                          //           fontSize: 12,
+                          //           height: 1.5,
+                          //           color: Color(0xFF34607B),
+                          //         ),
+                          //       ),
+                          //     ),
+                          //   ],
+                          // ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
+              // Padding(
+              //   padding: const EdgeInsets.only(left: 58.0),
+              //   child:
+              // PopupMenuButton(
+              //   itemBuilder: (BuildContext context) {
+              //     return [
+              //       PopupMenuItem(child: Text('Option 1')),
+              //       PopupMenuItem(child: Text('Option 2')),
+              //       PopupMenuItem(child: Text('Option 3')),
+              //     ];
+              //   },
+              // ),
+              // ),
             ],
           ),
-          // Padding(
-          //   padding: const EdgeInsets.only(left: 58.0),
-          //   child: 
-            // PopupMenuButton(
-            //   itemBuilder: (BuildContext context) {
-            //     return [
-            //       PopupMenuItem(child: Text('Option 1')),
-            //       PopupMenuItem(child: Text('Option 2')),
-            //       PopupMenuItem(child: Text('Option 3')),
-            //     ];
-            //   },
-            // ),
-          // ),
-        ],
-      ),
-    ));
+        ));
   }
 }
